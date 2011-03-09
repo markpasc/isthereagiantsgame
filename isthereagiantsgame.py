55484fd40aa1686bbe6f38adc7334837e8b198c2
Mark Paschal <markpasc@markpasc.org>
2011-03-09 11:50:33 -0800

    Just redirect

--- isthereagiantsgame.py
+++ isthereagiantsgame.py
@@ -1,46 +1,13 @@
 from google.appengine.dist import use_library
 use_library('django', '0.96')
 
-from datetime import datetime, timedelta
-from os.path import join, dirname
-from random import choice
-
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext.webapp import template
-
-from giants_schedule import schedule
 
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        now = datetime.utcnow() + timedelta(hours=-9)
-        today = now.date()
-
-        for game in schedule:
-            if game[0] >= today:
-                break
-        if game[0] < today:
-            game = None
-
-        data = {
-            'today': today,
-            'gametoday': game[0] == today,
-            'homegame': game[1].endswith('at San Francisco'),
-            'nextgame': game,
-        }
-
-        opening_day = datetime(year=2011, month=4, day=1).date()
-        if today < opening_day:
-            for homegame in schedule:
-                if homegame[0] >= opening_day and homegame[1].endswith('at San Francisco'):
-                    data['firsthomegame'] = homegame
-                    break
-
-        path = join(dirname(__file__), 'html', 'game.html')
-        html = template.render(path, data)
-        self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-        self.response.out.write(html)
+        self.redirect('http://isthereagiantsga.me/', permanent=True)
 
 
 routes = [
